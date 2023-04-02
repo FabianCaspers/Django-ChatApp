0f2e50a606b703f42aca31cfba90c66ff8736189
FabianCaspers <fabian.c1308@icloud.com>
2023-04-02 18:55:36 +0200

    add Message and Show Message

--- chat/admin.py
+++ chat/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
+from .models import Message
+
+
+class MessageAdmin(admin.ModelAdmin):
+    fields = ('text', 'createt_at', 'author', 'receiver')    
+    list_display = ('createt_at', 'author', 'text', 'receiver')    
+    search_fields = ('text',)
+
+
 # Register your models here.
+admin.site.register(Message, MessageAdmin)
