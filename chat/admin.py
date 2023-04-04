7a0b052c0c7246f22e71fdebd3b805426ed8ad3b
FabianCaspers <fabian.c1308@icloud.com>
2023-04-04 15:03:21 +0200

    add Chat and POST

--- chat/admin.py
+++ chat/admin.py
@@ -1,13 +1,14 @@
 from django.contrib import admin
 
-from .models import Message
+from .models import Message, Chat
 
 
 class MessageAdmin(admin.ModelAdmin):
-    fields = ('text', 'createt_at', 'author', 'receiver')    
+    fields = ('chat', 'text', 'createt_at', 'author', 'receiver')    
     list_display = ('createt_at', 'author', 'text', 'receiver')    
     search_fields = ('text',)
 
 
 # Register your models here.
 admin.site.register(Message, MessageAdmin)
+admin.site.register(Chat)
