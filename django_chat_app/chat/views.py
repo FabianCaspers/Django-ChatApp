ef436e88921ea35c98ff990895339b6e02c456a1
FabianCaspers <fabian.caspers1308@gmail.com>
2023-07-13 14:24:40 +0200

    login annotation

--- django_chat_app/chat/views.py
+++ django_chat_app/chat/views.py
@@ -2,8 +2,10 @@ from django.shortcuts import render
 from .models import Message, Chat
 from django.http.response import HttpResponseRedirect
 from django.contrib.auth import authenticate, login
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
+@login_required(login_url='/login/')
 def index(request):
     chatMessages = None
     if request.method == 'POST':
@@ -14,12 +16,14 @@ def index(request):
         chatMessages = Message.objects.filter(chat__id=1)
     return render(request, 'chat/index.html', {'messages': chatMessages})
 
+
 def login_view(request):
+    redirect = request.GET.get('next')
     if request.method == 'POST':
         user = authenticate(username=request.POST.get('username'), password=request.POST.get('password'))
         if user: 
             login(request, user)
-            return HttpResponseRedirect('/chat/')
+            return HttpResponseRedirect(request.POST.get('redirect'))
         else:
-            return render(request, 'auth/login.html', {'wrongPassword': True})
-    return render(request, 'auth/login.html')
+            return render(request, 'auth/login.html', {'wrongPassword': True, 'redirect': redirect})
+    return render(request, 'auth/login.html', {'redirect': redirect})
