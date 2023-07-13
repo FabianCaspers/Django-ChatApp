a9ca00c96b7dee996c4fb23617f2246b13769bcc
FabianCaspers <fabian.caspers1308@gmail.com>
2023-07-13 13:26:02 +0200

    Get, Post, Model Fields change

--- django_chat_app/chat/admin.py
+++ django_chat_app/chat/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Message, Chat
+
+class MessageAdmin(admin.ModelAdmin):
+    fields = ('text','createt_at', 'author', 'receiver')
+    list_display = ('createt_at', 'author', 'text', 'receiver')
+    search_fields = ('text',)
 
 # Register your models here.
+admin.site.register(Message, MessageAdmin)
+admin.site.register(Chat)
