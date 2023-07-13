a7348863ab1cb9ea541c6204d71d9be504d8d369
FabianCaspers <fabian.caspers1308@gmail.com>
2023-07-13 13:59:57 +0200

    authentication

--- django_chat_app/chat/views.py
+++ django_chat_app/chat/views.py
@@ -3,9 +3,11 @@ from .models import Message, Chat
 
 # Create your views here.
 def index(request):
+    chatMessages = None
     if request.method == 'POST':
         print("Reveived data " + request.POST['textmessage'])
         myChat = Chat.objects.get(id=1)
-        Message.objects.create(text=request.POST['textmessage'], chat=myChat, author=request.user, receiver=request.user)
+        author = request.user
+        Message.objects.create(text=request.POST['textmessage'], chat=myChat, author=request.user, receiver=author)
         chatMessages = Message.objects.filter(chat__id=1)
     return render(request, 'chat/index.html', {'messages': chatMessages})
